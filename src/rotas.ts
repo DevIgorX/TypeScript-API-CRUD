696e27175baf74c9d69dabb30d35520ebf93af34
Tauam Igor <tauamigor@gmail.com>
2023-10-02 18:15:42 -0300

    implementação do arquivo de controladores

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import{ atualizarCarros, cadastrarCarros, detalharCarros, exlcuirCarros, listarCarros } from   './controladores/carros'
 
 const rotas = Router()
 
 
-rotas.get('/')
+rotas.get('/carros', listarCarros)
+rotas.get('/carros/:id',detalharCarros)
+rotas.post('/carros', cadastrarCarros)
+rotas.put('/carros/:id',atualizarCarros)
+rotas.delete('/carros/:id',exlcuirCarros)
 
 
 export default rotas
